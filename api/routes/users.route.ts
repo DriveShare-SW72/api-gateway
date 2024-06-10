61b52394f61d104284ad516957a66a4c0faef08c
SgtGarro <marcelogarro137@gmail.com>
2024-06-09 23:10:46 -0500

    feat: add user service to api gateway

--- api/routes/users.route.ts
+++ api/routes/users.route.ts
@@ -1,9 +1,21 @@
 import express from 'express'
+import { createProxyMiddleware, fixRequestBody } from 'http-proxy-middleware'
+
+const API_TARGET_URL = 'https://backend-userservice-fa3y.onrender.com'
 
 const router = express.Router()
 
-router.get('/', (_req, res) => {
-  res.json({ message: 'Welcome to the User service' })
-})
+router.use(
+  '/',
+  createProxyMiddleware({
+    target: API_TARGET_URL,
+    secure: false,
+    changeOrigin: true,
+    logger: console,
+    on: {
+      proxyReq: fixRequestBody,
+    },
+  }),
+)
 
 export default router
